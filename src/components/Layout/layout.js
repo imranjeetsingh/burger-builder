4b46c6f35b47da973fa95785a9a95418a771a97c
imranjeetsingh <ranjee970@gmail.com>
2019-11-04 16:56:20 +0530

    added support for mobile devices

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-const Layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-             { props.children } 
-        </main>
-    </Aux>
-)
+import SideDrawer from '../../components/Navigation/SideDrawer/Sidedrawer';
+
+class Layout extends Component {
+
+    state = {
+        showSideDrawer : true 
+    }
+    sideDrawerCloseHandler = () =>{
+        this.setState({showSideDrawer : false})
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open = {this.state.showSideDrawer} closed = {this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
 export default Layout;
