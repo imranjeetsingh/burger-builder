8bfee38fb3d1f25b42084ec1e333075408ade4c4
imranjeetsingh <ranjee970@gmail.com>
2019-11-05 09:15:40 +0530

    added firebase data to storre customer order

--- src/containers/burgerbuulder/burgerbuilder.js
+++ src/containers/burgerbuulder/burgerbuilder.js
@@ -4,8 +4,7 @@ import Burger from '../../components/burger/burger';
 import BuildControls from '../../components/burger/buildscontrols/buildscontrols';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/burger/Ordersummary/Ordersummary';
-// import BuildControl from '../../components/burger/buildscontrols/buildcontrol/buildcontrol';
-
+import Axios from '../../axios-order';
 
 const INGREDIENTS_PRICES = {
     salad : 0.5,
@@ -36,7 +35,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinuehandler = () =>{
-        alert("Have your Burger!!!")
+        // alert("Have your Burger!!!")
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Ranjeet Singh",
+                address: {
+                    street: "poisar",
+                    zipcode: '400101',
+                    country: 'India'
+                },
+                email: "ranjee970@gmail.com",
+            },
+            deliveryMethod: 'fastest',
+        }
+        Axios.post("/orders.json",order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
 
     updatePurchaseable (ingredients) {
