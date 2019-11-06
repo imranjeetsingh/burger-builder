5e2bc6f5cb07a54ac32d14aa286c954b1d6cdd70
imranjeetsingh <ranjee970@gmail.com>
2019-11-06 20:30:01 +0530

    added redux support and initial set up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { createStore} from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import Reducer from './store/reducers';
+
+const store = createStore(Reducer);
 
 const NewApp =(
+    <Provider store = {store}>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(NewApp, document.getElementById('root'));
---
+++ src/store/reducers.js
@@ -0,0 +1,12 @@
+import * as ActionType from './actions';
+
+const initialState = {
+    ingrdients : null,
+    totalPrice : 4,
+}
+
+const Reducer = (state = initialState, action) => {
+
+}
+
+export default Reducer;
